8245108d5d5c9318156b8df1819d51f6e1be38c0
Nujke <mike.bellika97@gmail.com>
2016-04-27 17:32:38 +0200

    Added a logging function

--- functions.php
+++ functions.php
@@ -16,4 +16,16 @@ function generate_hash($password){
     $salt = '$2y$rNQH1uwlNOqRbzYhWeUa$' . substr(md5(uniqid(rand(), true)), 0, 22);
     return crypt($password, $salt);
 }
+
+function log_event($action, $ip, $user_id, $people_id){
+    global $mysqli;
+    $query = "INSERT INTO logs (action, ip, users_id, people_id) VALUES (?,?,?,?)";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param('ssii', $action, $ip, $user_id, $people_id);
+    if(!$stmt->execute()){
+        $stmt->close();
+        array_push($error, "Error code: #1");
+        return $error;
+    }
+}
 ?>
